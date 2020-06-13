d650897dede15330af8f6f56832d5eeecb7d077e
uvardan <uvardan@utexas.edu>
2020-06-13 14:32:56 -0500

    Problem 5 solution -- Needs Review

--- assignments/hw1/src/hw1.cpp
+++ assignments/hw1/src/hw1.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <omp.h>
 #include "hw1.h"
+#include <math.h>
 
 double euclidean_length(std::vector<double> vector) {
+
+  
   // Your code goes here.
-  return 0;
+  if(vector.size()==0){
+    return 0;
+  }
+  int i;
+  double sum=0;
+  double result=0;
+
+  #pragma opm parallel for shared(sum, vector) reduciton(+: sum) 
+  std:: cout<< "Number of therad: "<<omp_get_thread_num()<<"\n";
+  for(auto i=0;i<vector.size();i++){
+    sum += vector[i]*vector[i];
+  }
+  result=sqrt(sum);
+  return result;
 }
+
+
 std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector) {
   // Your code goes here
   return std::vector<int64_t>();
